c1a20fa54e92d32d77342d87a5103ac94a6e0234
TodorovicSrdjan <tsrdjan@pm.me>
2021-05-25 00:25:05 +0200

    Dodata rešenja II kolokvijuma (PiOS)
    - dodata rešenja zadataka II kolokvijuma II grupe iz OS1 (PowerShell), 2017-2018

---
+++ II godina/Letnji semestar/PiOS/Kolokvijumi/II_kolokvijum/2017-2018/Regularni/Resenja_studenata/Srdjan87-19/zadatak1.ps1
@@ -0,0 +1,39 @@
+﻿if( $args.Count -ne 1 ) {
+    Write-Output "Skripta prima tacno 1 parametar"
+    exit 1
+}
+
+$putanja = $args[0]
+
+if( !( Test-Path $putanja -PathType Leaf ) ) {
+    Write-Output "Neispravna putanja"
+    exit 2
+}
+
+$fajlovi = Get-ChildItem . -File
+
+$notRO = @()
+$roCount = 0
+$notROCount = 0
+
+$fajlovi.ForEach({
+    if( $_.isReadOnly ) {
+       $roCount++
+    }
+    else {
+        if( (Get-Date).addMonths(-1) -le $_.CreationTime ) {
+            $notRO += $_
+        }
+        $notROCount++
+    }
+})
+
+Write-Output "Broj fajlova po isReadOnly statusu`n" > $putanja 
+
+Write-Output "ReadOnly: False`nCount: $notROCount`n" >> $putanja 
+Write-Output "ReadOnly: True`nCount: $roCount`n" >> $putanja 
+
+Write-Output "Trenutni datum $(get-date -Format 'MM/dd/yyyy HH:mm:ss')" >> $putanja 
+Write-Output "Fajlovi koji nisu readOnly a kreirani su u poslednjih mesec dana su:" >> $putanja 
+
+Write-Output $notRO | Select-Object Name, Length, CreationTime >> $putanja 
---
+++ II godina/Letnji semestar/PiOS/Kolokvijumi/II_kolokvijum/2017-2018/Regularni/Resenja_studenata/Srdjan87-19/zadatak2.ps1
@@ -0,0 +1,84 @@
+﻿if( $args.Count -ne 2 ) {
+    Write-Output "Skripta prihvata tacno 2 argumenta"
+    exit 1
+}
+
+$dir1 = $args[0]
+$dir2 = $args[1]
+
+if( !( Test-Path $dir1 -PathType Container ) ) {
+    Write-Output "Prvi parametar nije direktorijum"
+    exit 2
+}
+
+if( !( Test-Path $dir2 -PathType Container ) ) {
+    Write-Output "Drugi parametar nije direktorijum"
+    exit 3
+}
+
+$ekstenzije = Get-ChildItem $dir1 -Recurse -File | foreach { if( $_.Extension -ne "" ) { return $_.Extension } } | Select-Object -Unique
+
+$prosek = 0
+$len = 0
+$count = 0
+
+$minProsek = 0
+$minProsekExt = $ekstenzije[0]
+
+$minLen = 0
+$minLenExt = $ekstenzije[0]
+
+$ekstenzije.ForEach( {
+    
+    $fajlovi = Get-ChildItem $dir1 -Filter "*$_" -Recurse -File
+
+    foreach( $fajl in $fajlovi ) {
+        $count++
+        $len += $fajl.Length
+    }
+
+    $lenGB = $len / 1000000000
+    $prosek = $lenGB / $count
+    Write-Output "Ekstenzija: $_`tProsek: $($prosek)GB`tUkupno: $($lenGB)GB"
+
+    if( $minProsek -gt $prosek ) {
+        $minProsek = $prosek
+        $minProsekExt = $_
+    }
+
+    if( $minLen -gt $len ) {
+        $minLen = $len
+        $minLenExt = $_
+    }
+
+    $prosek = 0
+    $len = 0
+    $count = 0
+} )
+
+
+$dirNameProsek = "PROSEK" + ( $minProsekExt.Replace(".", "").ToUpper() )
+$dirNameMin = "MIN" + ( $minLenExt.Replace(".", "").ToUpper() )
+
+if( !( Test-Path "$dir2\$dirNameProsek" -PathType Container ) ) {
+    New-Item -Path $dir2 -Name $dirNameProsek -ItemType Directory > $null
+}
+
+if( !( Test-Path "$dir2\$dirNameMin" -PathType Container ) ) {
+    New-Item -Path $dir2 -Name $dirNameMin -ItemType Directory > $null
+}
+
+$prosekFajlovi = Get-ChildItem $dir1 -Filter "*$minProsekExt" -Recurse -File
+$minFajlovi = Get-ChildItem $dir1 -Filter "*$minLenExt" -Recurse -File
+
+ echo $prosekFajlovi | foreach { 
+    if( !$_.IsReadOnly ) {
+        Copy-Item $_.FullName -Destination "$dir2\$dirNameProsek"
+    }
+}
+
+ echo $minFajlovi | foreach { 
+    if( !$_.IsReadOnly ) {
+        Copy-Item $_.FullName -Destination "$dir2\$dirNameMin"
+    }
+}
