872bbfb9656e96b6742aa3195e70e1884bda9121
TodorovicSrdjan <tsrdjan@pm.me>
2021-05-25 16:41:21 +0200

    Dodata rešenja domaćih zadatka (PiOS)
    - dodata rešenja domaćih zadatka sa moodle-a

---
+++ II godina/Letnji semestar/PiOS/Domaći/Domaći - Powershell - Moodle/Srdjan_87-19/zad1.ps1
@@ -0,0 +1,40 @@
+﻿if( $args.Count -lt 3 ) {
+    Write-Output "Skripta prima minimalno 3 parametra"
+    exit 1
+}
+
+$dir1 = $args[0]
+$dir2 = $args[1]
+
+if( !( Test-Path $dir1 -PathType Container ) ) { 
+    Write-Output "Prvi parametar ne predstavlja putanju do nekog direktorijuma"
+    exit 2
+}
+
+if( !( Test-Path $dir2 -PathType Container ) ) { 
+    Write-Output "Drugi parametar ne predstavlja putanju do nekog direktorijuma"
+    exit 3
+}
+
+if( (Convert-Path $dir1) -eq (Convert-Path $dir2) ) {
+    Write-Output "Prvi i drugi ne mogu biti ista putanja"
+    exit 4
+}
+
+for( $i=2; $i -lt $args.Count; $i++ ) {
+    $fajlovi = Get-ChildItem -Path $dir1 -Filter *.$($args[$i]) -Recurse -File
+
+    if( $fajlovi.Count -gt 0 ) {
+        if( Test-Path $dir2\$($args[$i]) -PathType Container ) {
+            Write-Output "Na putanji $dir2 vec postoji folder $($args[$i])"
+            Remove-Item $dir2\$($args[$i]) -Recurse -Confirm
+        }
+        else {
+            New-item -Path $dir2 -Name $($args[$i]) -ItemType Directory > $null
+
+            foreach( $fajl in $fajlovi ) {
+                Copy-Item $fajl.FullName -Destination $dir2\$($args[$i])
+            }
+        }
+    }
+}
---
+++ II godina/Letnji semestar/PiOS/Domaći/Domaći - Powershell - Moodle/Srdjan_87-19/zad2.ps1
@@ -0,0 +1,25 @@
+﻿if( $args.Count -ne 1 ) {
+    Write-Output "Skripta prihvata tacno 1 parametar"
+    exit 1
+}
+
+$putanja = $args[0]
+
+$fajlovi = Get-ChildItem . -File
+$nonROinfo = "Ime`tVelicina`tDatum kreiranja`n"
+$roNum = 0
+$nonROnum = 0
+
+foreach( $fajl in $fajlovi ) {
+    if( $fajl.IsReadOnly ) {
+        $roNum++
+    }
+    else {
+        $nonROnum++
+        $nonROinfo += $fajl.Name + "`t" + $fajl.Length + "`t" + $fajl.CreationTime + "`n"
+    }
+}
+
+Write-Output "Broj ReadOnly fajlova: $roNum" > $putanja
+Write-Output "Broj fajlova koji nisu ReadOnly: $nonROnum" >> $putanja
+Write-Output $nonROinfo >> $putanja
